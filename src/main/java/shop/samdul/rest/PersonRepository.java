1acf043dc8fd3a387e242a9950ac9f5632e00491
sooj1n <sujin19371@gmail.com>
2024-10-21 17:12:11 +0900

    PersonRepository

--- src/main/java/shop/samdul/rest/PersonRepository.java
+++ src/main/java/shop/samdul/rest/PersonRepository.java
@@ -1,4 +1,13 @@
 package shop.samdul.rest;
 
-public class PersonRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path="people")
+public interface PersonRepository extends
+        PagingAndSortingRepository<Person, Long>, CrudRepository<Person, Long> {
+
+
 }
+
